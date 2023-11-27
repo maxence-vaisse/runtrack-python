7ec01fcbe90433824837d1d3fc9c769d6c206005
maxence-vaisse <maxence.vaisse@laplateforme.io>
2023-11-27 09:09:43 +0100

    Runtrack Jour03 Job07

---
+++ jour03/Job07/main.py
@@ -0,0 +1,17 @@
+def idioma(langage):
+    if langage == "JavaScript":
+        print("tu es un développeur web")
+    elif langage == "python":
+        print("tu es un développeur IA")
+    elif langage == "java":
+        print("tu es un développeur logiciel")
+    elif langage == "reactjs":
+        print("tu es un developpeur mobile")
+    else:
+        print("un jour, je serai le meilleur développeur... ")
+
+idioma("python")
+idioma("JavaScript")
+idioma("java")
+idioma("reactjs")
+idioma("Pokdev")
