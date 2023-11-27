d6f4cd62d37690d7d2fe968ab3fde7835bb9f0a0
maxence-vaisse <maxence.vaisse@laplateforme.io>
2023-11-27 09:08:24 +0100

    Runtrack Jour03 Job06

---
+++ jour03/Job06/main.py
@@ -0,0 +1,11 @@
+def positif_ou_pas(nombre):
+    if nombre>0:
+        print("positif")
+    elif nombre<0:
+        print("négatif")
+    else:
+        print("nombre est égal à 0")
+
+positif_ou_pas(0)
+positif_ou_pas(3)
+positif_ou_pas(-3)
